ede3badf6e36ac0e93ac793ba297b166f7de2acc
ecln30 <ecln30@gmail.com>
2022-08-24 16:53:41 +0900

    add search

---
+++ pages/search.js
@@ -0,0 +1,119 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import {useState} from 'react'
+import {useRouter} from 'next/router'
+import Image from 'next/image'
+import {Flex,Box,Text,Icon} from '@chakra-ui/react'
+import {BsFilter} from 'react-icons/bs'
+
+const Search = () => {
+    const [serchFilters, setSearchFilters] = useState(false)
+    const router = useRouter()
+
+    return (
+        <Box>
+            <Flex 
+            cursor='pointer'
+            bg='gray.100'
+            >
+
+            </Flex>
+        </Box>
+    )
+}
+
+
+export default Search
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
