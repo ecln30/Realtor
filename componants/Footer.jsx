27281904db3a8006a6bb468af03a772b5643ada7
ecln30 <ecln30@gmail.com>
2022-08-25 12:31:46 +0900

    change filter

--- componants/Footer.jsx
+++ componants/Footer.jsx
@@ -16,7 +16,7 @@ import {Box} from '@chakra-ui/react'
 
 
 const Footer = () => (
-    <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.l00' >
+    <Box textAlign='center' p='5' color='gray.600' borderTop='' borderColor='gray.100' >
      2022 Realtor, Inc.  
     </Box>
 )
